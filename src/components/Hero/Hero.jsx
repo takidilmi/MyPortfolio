bfefc2941d8b5f755e0448d69fd647a0826e6e04
takidilmi <angeldilmi@gmail.com>
2023-12-01 15:14:17 +0100

    basic hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="flex flex-row flex-wrap-reverse justify-around items-center text-white">
+        <div className="text-[30px] break-words w-[65vw]">
+          <p>
+            Hi, my name is <b>Takieddine Dilmi</b>, a talented frontend web
+            developer.
+          </p>
+          <p className="opacity-80">let me show you...</p>
+        </div>
+        <div className="w-64 h-64 rounded-full overflow-hidden flex items-center justify-center">
+          <Image
+            src="/images/Takidilmi.jpg"
+            alt="Takidilmi"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
