1e04479e509dff1055d5578bbbd63919251c3508
takidilmi <angeldilmi@gmail.com>
2023-12-01 22:42:49 +0100

    add techs and projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,47 @@
 import React, { useRef, useEffect } from "react";
 import Image from "next/image";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 
 const images = [
   {
-    src: "/images/Takidilmi.jpg",
-    alt: "Image 1",
+    src: "/projects/Pebble.png",
+    alt: "https://pebble-work.vercel.app/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    tech1: "/Tech/icons8-nextjs.svg",
+    tech2: "/Tech/icons8-firebase.svg",
+    tech3: "/Tech/material-ui.svg",
   },
   {
-    src: "/images/Takidilmi.jpg",
-    alt: "Image 2",
+    src: "/projects/Sougna.png",
+    alt: "https://sougna.vercel.app/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    tech1: "/Tech/icons8-nextjs.svg",
+    tech2: "",
+    tech3: "",
   },
   {
-    src: "/images/Takidilmi.jpg",
-    alt: "Image 3",
+    src: "/projects/Madlibs.png",
+    alt: "https://202306-nea-dz-few.github.io/madlibs-911/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    tech1: "/Tech/icons8-nextjs.svg",
+    tech2: "",
+    tech3: "",
   },
   {
-    src: "/images/Takidilmi.jpg",
-    alt: "Image 4",
+    src: "/projects/Movie.png",
+    alt: "https://reelio-topaz.vercel.app/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    tech1: "/Tech/icons8-nextjs.svg",
+    tech2: "",
+    tech3: "",
+  },
+  {
+    src: "/projects/Lemon.png",
+    alt: "Lemon",
+    text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    tech1: "/Tech/icons8-nextjs.svg",
+    tech2: "",
+    tech3: "",
   },
 ];
 const Projects = () => {
@@ -42,23 +62,28 @@ const Projects = () => {
               style={{ zIndex: 1 }}
               onHoverStart={() => {
                 shadowPRefs.current[index].forEach((p) => {
-                  p.style.boxShadow = "3px 3px 3px rgba(0, 0, 0, 0.5)";
+                  p.style.backdropFilter = "blur(10px)";
                 });
               }}
               onHoverEnd={() => {
                 shadowPRefs.current[index].forEach((p) => {
-                  p.style.boxShadow = "none";
+                  p.style.backdropFilter = "none";
                 });
               }}>
               <InteractiveImage src={image.src} alt={image.alt} />
-              <p ref={el => shadowPRefs.current[index] = [...(shadowPRefs.current[index] || []), el]} className="text-center bg-red-600 absolute top-0 shadow-p">
-                {image.alt}
-              </p>
-              <p ref={el => shadowPRefs.current[index] = [...(shadowPRefs.current[index] || []), el]} className="text-center bg-red-600 absolute top-5 shadow-p">
-                {image.alt}
-              </p>
-              <p ref={el => shadowPRefs.current[index] = [...(shadowPRefs.current[index] || []), el]} className="text-center bg-red-600 absolute top-10 shadow-p">
-                {image.alt}
+              <p
+                ref={(el) =>
+                  (shadowPRefs.current[index] = [
+                    ...(shadowPRefs.current[index] || []),
+                    el,
+                  ])
+                }
+                className="text-center flex absolute top-0 shadow-p">
+                <Image src={image.tech1} width={30} height={30} alt="" />
+
+                <Image src={image.tech1} width={30} height={30} alt="" />
+
+                <Image src={image.tech1} width={30} height={30} alt="" />
               </p>
             </motion.div>
             <p
@@ -72,7 +97,7 @@ const Projects = () => {
       </div>
       <style jsx>{`
         .shadow-p {
-          transition: box-shadow 0.3s ease-in-out;
+          transition: backdrop-filter 0.2s ease-in-out;
         }
       `}</style>
     </>
@@ -83,40 +108,55 @@ const InteractiveImage = ({ src, alt }) => {
   const imageRef = useRef(null);
   let animationFrameId = null;
 
-  const handleMouseMove = (event) => {
+  const handleInteractionMove = (event) => {
     if (animationFrameId) {
       cancelAnimationFrame(animationFrameId);
     }
 
     animationFrameId = requestAnimationFrame(() => {
-      let { left, top, width, height } =
-        imageRef.current.getBoundingClientRect();
-      let x = ((event.clientX - left) / width - 0.5) * 1.5;
-      let y = ((event.clientY - top) / height - 0.5) * 1.5;
-      imageRef.current.style.transform = `perspective(500px) rotateX(${
-        -y * 10
-      }deg) rotateY(${x * 10}deg)`;
+      if (imageRef.current) {
+        let { left, top, width, height } =
+          imageRef.current.getBoundingClientRect();
+        let x, y;
+        if (event.type === "mousemove") {
+          x = ((event.clientX - left) / width - 0.5) * 1.5;
+          y = ((event.clientY - top) / height - 0.5) * 1.5;
+        } else if (event.type === "touchmove") {
+          x = ((event.touches[0].clientX - left) / width - 0.5) * 1.5;
+          y = ((event.touches[0].clientY - top) / height - 0.5) * 1.5;
+        }
+        imageRef.current.style.transform = `perspective(500px) rotateX(${
+          -y * 10
+        }deg) rotateY(${x * 10}deg)`;
+      }
     });
   };
-  const handleMouseLeave = () => {
+
+  const handleInteractionEnd = () => {
     if (animationFrameId) {
       cancelAnimationFrame(animationFrameId);
     }
 
     animationFrameId = requestAnimationFrame(() => {
-      imageRef.current.style.transition = "transform 0.2s linear";
-      imageRef.current.style.transform =
-        "perspective(500px) rotateX(0) rotateY(0)";
+      if (imageRef.current) {
+        imageRef.current.style.transition = "transform 0.2s linear";
+        imageRef.current.style.transform =
+          "perspective(500px) rotateX(0) rotateY(0)";
+      }
     });
   };
 
   useEffect(() => {
     const imageElement = imageRef.current;
-    imageElement.addEventListener("mousemove", handleMouseMove);
-    imageElement.addEventListener("mouseleave", handleMouseLeave);
+    imageElement.addEventListener("mousemove", handleInteractionMove);
+    imageElement.addEventListener("mouseleave", handleInteractionEnd);
+    imageElement.addEventListener("touchmove", handleInteractionMove);
+    imageElement.addEventListener("touchend", handleInteractionEnd);
     return () => {
-      imageElement.removeEventListener("mousemove", handleMouseMove);
-      imageElement.removeEventListener("mouseleave", handleMouseLeave);
+      imageElement.removeEventListener("mousemove", handleInteractionMove);
+      imageElement.removeEventListener("mouseleave", handleInteractionEnd);
+      imageElement.removeEventListener("touchmove", handleInteractionMove);
+      imageElement.removeEventListener("touchend", handleInteractionEnd);
     };
   }, []);
 
@@ -128,7 +168,14 @@ const InteractiveImage = ({ src, alt }) => {
         transformStyle: "preserve-3d",
         transition: "transform 50ms linear",
       }}>
-      <Image src={src} alt={alt} layout="responsive" width={500} height={300} />
+      <Image
+        className="object-cover max-h-[200px]"
+        src={src}
+        alt={alt}
+        layout="responsive"
+        width={500}
+        height={300}
+      />
     </div>
   );
 };
