4cf52d63d399815c80d2eac544e65a175ae91c69
takidilmi <angeldilmi@gmail.com>
2023-12-03 20:14:12 +0100

    creat a navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
+import Image from "next/image";
 import React from "react";
+import { TfiGithub } from "react-icons/tfi";
+import { IoLogoLinkedin } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <>
-      <div>
-        <p>TAKI</p>
+      <div className="sticky top-0 z-[999]">
+        <div className="text-white flex justify-around relative">
+          <div className="flex justify-between gap-20 p-2 flex-wrap text-[25px]">
+            <a
+              className="flex gap-2 flex-wrap items-center"
+              href="https://www.linkedin.com/in/takidilmi/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <IoLogoLinkedin size={30} />
+              <p>Linkedin</p>
+            </a>
+            <a
+              className="flex gap-2 items-center"
+              href="https://github.com/takidilmi"
+              target="_blank"
+              rel="noopener noreferrer">
+              <p>Github</p>
+              <TfiGithub size={30} />
+            </a>
+          </div>
+          <Image
+            className="absolute top-[-20px]"
+            src="/logo.svg"
+            width={200}
+            height={200}
+            alt="logo"
+          />
+        </div>
       </div>
     </>
   );
