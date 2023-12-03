09d3f7467126355174a07e4fdcbf7f5628ca8b7d
takidilmi <angeldilmi@gmail.com>
2023-12-03 16:50:25 +0100

    add projects description

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,12 +2,13 @@ import React, { useRef, useEffect } from "react";
 import Image from "next/image";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import Link from "next/link";
+import { TfiGithub } from "react-icons/tfi";
+import { PiFileHtmlLight } from "react-icons/pi";
 
 const images = [
   {
     src: "/projects/Pebble.png",
-    repository:"https://github.com/202306-NEA-DZ-FEW/Pebble-work",
+    repository: "https://github.com/202306-NEA-DZ-FEW/Pebble-work",
     alt: "PebbleWork",
     link: "https://pebble-work.vercel.app/",
     text: "Pebble Work is a capstone project that showcases my skills and creativity as a web developer. It is a platform that connects volunteers and organizers, allowing them to find and work on projects that are aligned with the 17 Sustainable Development Goals (SDGs). I worked with a team of five to create this project, and I was responsible for the front-end development. We communicated and coordinated with each other using Discord, GitHub, and Zoom, and we followed the agile methodology and the Git flow. I used Next.js, Firebase, i18next, Material UI, Tailwind, and vanilla CSS to create a responsive and dynamic website that supports multiple languages and animations. I also added tests and snapshots to the components using Jest, and used Git action as my deployment tool. I'm proud of this project because it demonstrates my ability to create a full-stack web application that contributes to a better and more sustainable future for all.",
@@ -19,17 +20,18 @@ const images = [
   },
   {
     src: "/projects/Sougna.png",
-    repository:"https://github.com/202306-NEA-DZ-FEW/e-commerce-project-buy-or-die",
+    repository:
+      "https://github.com/202306-NEA-DZ-FEW/e-commerce-project-buy-or-die",
     alt: "Sougna",
     link: "https://sougna.vercel.app/",
-    text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
+    text: "Sougna is an e-commerce project that I created with five other team members as part of our web development course. It is a platform that provides a seamless online shopping experience for users and a convenient product management and order processing system for the admin. We used NextJs, Firebase, Redux and Stripe to build this project. We also implemented user authentication and registration, product catalog with search and filtering options, shopping cart functionality, secure payment processing, user reviews and ratings, and admin dashboard for managing products and orders. We made sure that the website is responsive and accessible on various devices, and we added some animations and transitions to enhance the user interface. I was responsible for the front-end development and the user interface design, and I used Next.js, Tailwind, and other technologies to create a modern and elegant website. I was also honored to lead this team for a week, and I coordinated the tasks, deadlines, and feedback among the team members. We communicated and collaborated with each other using GitHub, Discord, and Zoom, and we deployed the website on Vercel. I'm proud of this project because it demonstrates my ability to create a full-stack web application that solves a real-world problem and provides a great user experience.",
     tech1: "/Tech/icons8-nextjs.svg",
     tech2: "/Tech/icons8-tailwind-css.svg",
     tech3: "/Tech/icons8-firebase.svg",
   },
   {
     src: "/projects/Madlibs.png",
-    repository:"https://github.com/202306-NEA-DZ-FEW/madlibs-911",
+    repository: "https://github.com/202306-NEA-DZ-FEW/madlibs-911",
     alt: "Madlibs",
     link: "https://202306-nea-dz-few.github.io/madlibs-911/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
@@ -39,7 +41,7 @@ const images = [
   },
   {
     src: "/projects/Movie.png",
-    repository:"https://github.com/202306-NEA-DZ-FEW/movie-project-devsquad",
+    repository: "https://github.com/202306-NEA-DZ-FEW/movie-project-devsquad",
     alt: "Relio",
     link: "https://reelio-topaz.vercel.app/",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
@@ -49,7 +51,7 @@ const images = [
   },
   {
     src: "/projects/Lemon.png",
-    repository:"https://github.com/takidilmi/little-lemon",
+    repository: "https://github.com/takidilmi/little-lemon",
     alt: "Lemon",
     link: "",
     text: "coming soon xD why not just testing out my writing speed as yah i got experienced a lot and this is amazing i can thing and type fast",
@@ -99,7 +101,7 @@ const Projects = () => {
                 className="w-1/2 relative p-3 rounded-[6px] overflow-hidden"
                 whileHover={{
                   scale: 1.1,
-                  backdropFilter: "blur(10px)",
+                  backdropFilter: "blur(2px)",
                   transition: { duration: 0.5 },
                 }}
                 transition={{
@@ -141,9 +143,20 @@ const Projects = () => {
                 }`}>
                 <div className="flex justify-between items-center">
                   <h3 className="text-[18px] font-[600]">{image.alt}</h3>
-                  <div>
-                    <Link href={image.link}></Link>
-                    <Link href={image.link}>LINK</Link>
+                  <p className="opacity-50">Visit Links =&gt; </p>
+                  <div className="flex gap-5">
+                    <a
+                      href={image.repository}
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <TfiGithub size={30} />
+                    </a>
+                    <a
+                      href={image.link}
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <PiFileHtmlLight size={30} />
+                    </a>
                   </div>
                 </div>
                 <br></br>
